047502d22fa8e1a20ee4a809e252f8367602ee15
JulioMora <juliocmorab@hotmail.com>
2017-05-03 15:09:57 -0500

    cree funciones

---
+++ 02 Javascript/Funciones/funciones.js
@@ -0,0 +1,47 @@
+/**
+ * Created by julio_000 on 03/05/2017.
+ */
+
+function holaMundo() {
+    console.log('Hola Mundo');
+}
+
+holaMundo();
+
+function Doble(numero) {
+return numero*2;
+}
+function multiplicar2(a,b) {
+    return a*b;
+}
+var dos=multiplicar2(1,2);
+var multiplicar=multiplicar2;
+console.log(Doble(2));
+console.log(multiplicar2(4,3));
+console.log(multiplicar2(6,4));
+console.log(multiplicar(6,3));
+
+var hola=function () { //funcion anonima void
+
+    console.log('hola')
+}
+var resultado=hola()
+console.log(resultado);
+console.log('funcion',holaMundo);
+
+function hazAlgo(algo) {
+    algo();
+}
+hazAlgo(function () {
+    console.log('Hola')
+})
+
+function TransformarNumero(numero,tranformacion) {
+    tranformacion(numero);
+}
+TransformarNumero(5,function (numero) {
+    return numero*2;
+})
+TransformarNumero(8,function (numero) {
+    return numero/2;
+})
--- 02 Javascript/Variables/variables.js
+++ 02 Javascript/Variables/variables.js
@@ -52,10 +52,64 @@ var Julio=  {
     altura:1.78,
     negativo:-1,
     mascota:{
-        nombre:tango,
+        nombre:'tango',
         fechaNacimiento: new Date()
     }
 
 };
 
 console.log(Julio,"Hola","1","2");
+console.log(Julio.mascota.fechaNacimiento);
+console.log(Julio.altura);
+Julio.altura=1.48;
+
+console.log(Julio.altura);
+Julio.altura={
+    ancho:2.4,
+    alto:1.78,
+}
+console.log(Julio.altura);
+delete Julio.altura;
+Julio.juegos='Warcraft'
+console.log("objeto",Julio);
+
+//arreglos
+
+var arreglo=[1,
+    1,
+    'Julio',
+    2.4,
+    true,
+    null,
+    undefined,
+    -454,
+    new Date(),
+    {a:'b'}];
+
+var nuevoObjeto={
+    papas:['chola','chaucha','maria'],
+    cebollas:['perla','paiteña','blanca']
+}
+
+console.log(arreglo);
+console.log(arreglo[4]);
+
+var matriz=[[0,1],[2,3]];
+console.log(matriz[0][0]);
+console.log(nuevoObjeto.papas[1]);
+
+var arregloObjeto=[
+    {id:1, nombre1:'Julio'},
+    {id:2, nombre1:'Felipe'}
+];
+// splice(
+// 'Delimita la posisicion en la que vamos a trabajar',
+// 'Numero Objetos a eliminarse')
+
+console.log('Antes:',arregloObjeto)
+arregloObjeto.splice(0,1)
+console.log('Despues:',arregloObjeto)
+
+arregloObjeto.splice(0,0,{id:3,nombre:'Carmen'})
+console.log('Despues2:',arregloObjeto)
+
