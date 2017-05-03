7cc81b974672d2ac8f2eeb35c362925710c84a35
JulioMora <juliocmorab@hotmail.com>
2017-05-03 13:23:47 -0500

    1er commit

---
+++ 02 Javascript/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by julio_000 on 03/05/2017.
+ */
+
+//aqui escribi para imprimir en consola
+console.log("hello world y se la creyo");
--- Aplicacion1/config/models.js
+++ Aplicacion1/config/models.js
@@ -18,7 +18,7 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
   // connection: 'localDiskDb',
-
+   connection: 'conexionMysqldocker',
   /***************************************************************************
   *                                                                          *
   * How and whether Sails will attempt to automatically rebuild the          *
