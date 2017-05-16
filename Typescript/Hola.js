60b91272b4bfda42da7a8c0bf7e217aec4992557
JulioMora <julio.mora@epn.edu.ec>
2017-05-16 16:27:25 -0500

    Objetos typescript

--- Typescript/Hola.js
+++ Typescript/Hola.js
@@ -14,4 +14,54 @@ var frase = "El nummero de novias es " + (novias + 1);
 //arrays
 var notas = [1, 2, 3, 4, 5, 6]; //
 var jaladad = [1, 2, 3];
+var nombre = "asd"; //any sirve para cualquier tipo de dato
+nombre = 12;
+var usuario = {
+    nombre: "Julio",
+    apellido: "Mora",
+    direccion: "Monjas"
+};
+var Persona = (function () {
+    function Persona(nombre, apellido, fechaNacimieto) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimieto = fechaNacimieto;
+    }
+    Persona.prototype.imprimirpersona = function () {
+        console.log(this.nombre, this.apellido, this.fechaNacimieto);
+    };
+    Persona.prototype.imprimirPersona2 = function () {
+        console.log(this.nombre, this.apellido);
+        this.funcion1('Julio');
+        return {
+            nombre: this.nombre,
+            apellido: this.apellido
+        };
+    };
+    Persona.prototype.funcion1 = function (nombre, apellido) {
+        if (apellido) {
+            console.log(nombre, apellido);
+        }
+        else {
+            console.log(nombre);
+        }
+    };
+    return Persona;
+}());
+function sumanumeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+var objeto = {
+    nombre: "julio",
+    imprimirNombre: function () {
+        return this.nombre;
+    }
+};
+var julio = new Persona();
+julio.nombre = "julio";
+julio.apellido = "mora";
+julio.fechaNacimieto = new Date("1994-04-06");
+//console.log(julio.nombre);
+julio.imprimirpersona();
+julio.imprimirPersona2();
 //# sourceMappingURL=Hola.js.map
--- Typescript/Hola.ts
+++ Typescript/Hola.ts
@@ -18,3 +18,67 @@ let frase =`El nummero de novias es ${novias+1}`;
 //arrays
 let notas:number[]=[1,2,3,4,5,6]//
 let jaladad:Array<number>=[1,2,3]
+
+let nombre:any="asd"//any sirve para cualquier tipo de dato
+nombre=12;
+
+let usuario:objetousuario={//implementa la intrerface con :
+    nombre:"Julio",
+    apellido:"Mora",
+    direccion:"Monjas"
+}
+
+interface objetousuario{
+    nombre:string,
+    apellido:string,
+    direccion?:string,
+}
+
+class Persona{
+    nombre:string;// publico por defecto
+    apellido:string;
+    public fechaNacimieto:Date;
+    constructor(nombre?: string, apellido?: string, fechaNacimieto?: Date) {//? significa que es opcional
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimieto = fechaNacimieto
+    }
+    imprimirpersona(){//detecta automatico void
+        console.log(this.nombre,this.apellido, this.fechaNacimieto);
+    }
+    imprimirPersona2():objetousuario{
+        console.log(this.nombre,this.apellido);
+        this.funcion1('Julio');
+        return{
+            nombre:this.nombre,
+            apellido:this.apellido
+        }
+    }
+    private funcion1(nombre:string,apellido?):void{
+        if(apellido){
+            console.log(nombre,apellido);
+        }else{
+            console.log(nombre);
+        }
+
+    }
+}
+
+function sumanumeros(numero1:number,numero2:number):number{
+    return numero1+numero2;
+}
+
+var objeto={
+    nombre:"julio",
+    imprimirNombre:function () {
+        return this.nombre;
+    }
+};
+
+let julio:Persona=new Persona();
+julio.nombre="julio";
+julio.apellido="mora";
+julio.fechaNacimieto=new Date("1994-04-06")
+//console.log(julio.nombre);
+julio.imprimirpersona();
+julio.imprimirPersona2()
