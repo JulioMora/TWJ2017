55e04b24f1587d877add0bfb39cebd5f669eb759
JulioMora <julio.mora@epn.edu.ec>
2017-05-16 15:21:20 -0500

    Primmer commit

---
+++ Typescript/Hola.js
@@ -0,0 +1,6 @@
+/**
+ * Created by julio_000 on 16/05/2017.
+ */
+var holamundo = "que tal mundo curel";
+console.log('hola mundo');
+//# sourceMappingURL=Hola.js.map
---
+++ Typescript/Hola.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by julio_000 on 16/05/2017.
+ */
+let holamundo=`que tal mundo curel`;
+console.log('hola mundo')
