2937323e03c0902f6727aa9c0096ccd1b536874b
JulioMora <julio.mora@epn.edu.ec>
2017-05-16 15:34:05 -0500

    Uso de variables typescript

--- Typescript/Hola.js
+++ Typescript/Hola.js
@@ -1,6 +1,17 @@
 /**
  * Created by julio_000 on 16/05/2017.
  */
-var holamundo = "que tal mundo curel";
-console.log('hola mundo');
+/*let holamundo=`que tal mundo curel`;
+console.log('hola mundo')*/
+//booleanos
+var soltero = true; //
+//number
+var hexadecimal = 0xf00;
+var novias = 7;
+var cadena = 'rojo';
+var adevertencia = "You're done";
+var frase = "El nummero de novias es " + (novias + 1);
+//arrays
+var notas = [1, 2, 3, 4, 5, 6]; //
+var jaladad = [1, 2, 3];
 //# sourceMappingURL=Hola.js.map
--- Typescript/Hola.ts
+++ Typescript/Hola.ts
@@ -1,5 +1,20 @@
 /**
  * Created by julio_000 on 16/05/2017.
  */
-let holamundo=`que tal mundo curel`;
-console.log('hola mundo')
+/*let holamundo=`que tal mundo curel`;
+console.log('hola mundo')*/
+
+//booleanos
+let soltero:boolean= true; //
+
+//number
+let hexadecimal:number=0xf00;
+let novias:number=7;
+let cadena: string='rojo';
+let adevertencia:string="You're done";
+
+let frase =`El nummero de novias es ${novias+1}`;
+
+//arrays
+let notas:number[]=[1,2,3,4,5,6]//
+let jaladad:Array<number>=[1,2,3]
